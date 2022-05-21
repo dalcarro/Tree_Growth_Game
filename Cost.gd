468e22a17408fa1e6992745077285e0a164a716c
David L. Carroll <93415053+ErdrickBelmont@users.noreply.github.com>
2022-05-21 14:21:55 -0700

    First tree game commit

---
+++ Cost.gd
@@ -0,0 +1,38 @@
+extends RichTextLabel
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Roots_mouse_entered():
+	self.set_text("Cost:")
+
+func _on_Trunk_mouse_entered():
+	self.set_text("Cost:")
+
+func _on_Branch_mouse_entered():
+	self.set_text("Cost:")
+
+func _on_Buds_mouse_entered():
+	self.set_text("Cost:")
+
+func _on_Water_mouse_entered():
+	self.set_text("Cost:")
+
+func _on_Sugar_mouse_entered():
+	self.set_text("Cost:")
+
+func _on_Caffine_mouse_entered():
+	self.set_text("Cost:")
---
+++ Description.gd
@@ -0,0 +1,43 @@
+extends RichTextLabel
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _on_Roots_mouse_entered(): 
+	self.set_text("Generates minerals and water every turn")
+
+func _on_Trunk_mouse_entered():
+	self.set_text("Increases Branch Cap")
+
+func _on_Branch_mouse_entered():
+	self.set_text("Increases Leaf Cap")
+
+func _on_Leaf_mouse_entered():
+	self.set_text("Generates sun")
+
+func _on_Buds_mouse_entered():
+	self.set_text("Generates minerals and water every turn")
+
+func _on_Water_mouse_entered():
+	self.set_text("Generates minerals and water every turn")
+
+func _on_Minerals_mouse_entered():
+	self.set_text("Generates minerals and water every turn")
+
+func _on_Sugar_mouse_entered():
+	self.set_text("Generates minerals and water every turn")
+
+func _on_Caffine_mouse_entered():
+	self.set_text("Generates minerals and water every turn")
