bd98fff0ce3e8e5520dfb3b466431ad011e9d9c9
David L. Carroll <dalcarro@ucsc.edu>
2022-05-23 22:28:31 -0700

    streamlined interface and added core script

---
+++ Node2D.gd
@@ -0,0 +1,55 @@
+extends Control
+
+
+#resource variables
+
+var water: int = 0
+var mineral: int = 0
+var sugar: int = 0
+var buds: int = 0
+var caff: int = 0
+
+#structure variables
+var trunk:int = 0
+var branch:int = 0
+var roots:int = 0
+var leaves:int = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+
+func _on_Roots_pressed():
+	pass # Replace with function body.
+
+
+func _on_Trunk_pressed():
+	pass # Replace with function body.
+
+
+func _on_Branch_pressed():
+	pass # Replace with function body.
+
+
+func _on_Leaf_pressed():
+	pass # Replace with function body.
+
+
+func _on_Buds_pressed():
+	pass # Replace with function body.
+
+
+func _on_Caffine_pressed():
+	pass # Replace with function body.
+
+
+func _on_Sugar_pressed():
+	pass # Replace with function body.
+
+
+func _on_end_turn_pressed():
+	pass # Replace with function body.
